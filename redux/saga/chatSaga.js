84d9dbb6bc901e71d240d25587ac4f1a77b0230f
Wasupol Vannasawang <wasupol.v@gmail.com>
2018-08-21 14:23:05 +0700

    add file chatSaga

---
+++ redux/saga/chatSaga.js
@@ -0,0 +1,28 @@
+import {takeLatsest,all,call,put} from 'redux-saga/effexts'
+import {
+    FETCH_CHAT,
+    SEND_MESSAGE,
+    fetchChat,
+    fetchChatSuccess,
+    fetchChatFailure,
+    sendMessage,
+    sendMessageSuccess,
+    sendMessageFailure
+} from '../duck/chat'
+import axios from 'axios'
+import {
+    API_SERVER,
+    getJSON,
+    patchJSON,
+    postJSON,
+    deleteJSON
+  } from '../../tool/api'
+
+export function* fetchChatSaga(action){
+    try {
+
+    }
+    catch {
+
+    }
+}
